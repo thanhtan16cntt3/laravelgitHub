5928ca822a8629afbbd7c943ac8c599d67a42164
thanhtan16cntt3 <dv.nttan98@gmail.com>
2020-08-07 08:31:49 +0700

    add pages test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.index');
 });
+Route::get('/test', function () {
+    return view('admin.pages.test');
+});
